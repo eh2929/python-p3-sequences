91db6ae0c14b12f103163f2749684a5eb03f5a22
Ethan <ethanhough29@gmail.com>
2024-01-30 15:51:37 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+    result = []
+    for _ in range(length):
+        result.append(a)
+        a, b = b, a + b
+    print(result)
